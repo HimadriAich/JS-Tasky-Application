3f5a08abd55b2e65463e45063f7ae3747b9c0622
HimadriAich <ajantaaich123@gmail.com>
2026-01-08 05:08:17 +0530

    nodeServ

--- server.js
+++ server.js
@@ -27,9 +27,70 @@ http.createServer((req, res) => {      // callback function to handle incoming r
    // This is a basic way to handle different routes and methods in a Node.js HTTP server
 if(url === "/todos"){
     if(method === "GET"){
-        res.writeHead(200);     //writehead is used to write the response header with status code 200 (OK) 
+        res.writeHead(200, {'Content-Type': 'text/html'});     //writehead is used to write the response header with status code 200 (OK) 
         res.write(toDoList.toString());      //write is used to write the response body with the toDoList data converted to a string
     }
+
+    else if(method === "POST"){
+        let body = "";          // Variable to store the incoming data
+        req.on('error', (err) => {       // Handling any errors during the request  (callback function for 'error' event)
+            console.log(err);     // Log any errors that occur during the request
+        }).on('data', (chunk) => {     // Listening for data chunks from the request  (callback function for 'data' event)
+            body += chunk;   // Append each chunk of data to the body variable
+            console.log(chunk);
+        }).on('end', () => {        // When all data has been received    (callback function for 'end' event)
+            body= JSON.parse(body);      // Parsing the received data as JSON
+           
+            let newToDo = toDoList;     // Reference to the existing toDoList array 
+            newToDo.push(body.item);        // Adding the new to-do item to the toDoList array
+            console.log(newToDo);    // Logging the updated toDoList to the console
+            //  console.log("data: ", body);            // Logging the received data to the console
+        }); 
+    }
+
+    else if(method === "DELETE"){
+        let body = "";          // Variable to store the incoming data
+        req.on('error', (err) => {       // Handling any errors during the request  (callback function for 'error' event)
+            console.log(err);     // Log any errors that occur during the request
+        }).on('data', (chunk) => {     // Listening for data chunks from the request  (callback function for 'data' event)
+            body += chunk;   // Append each chunk of data to the body variable
+        }).on('end', () => {        // When all data has been received    (callback function for 'end' event)
+            body= JSON.parse(body);      // Parsing the received data as JSON
+            //we need to remove the item from the toDoList array
+            let deleteThisItem= body.item;
+            for(let i=0; i<toDoList.length; i++){
+                if(toDoList[i] === deleteThisItem){
+                    toDoList.splice(i, 1);   // Removing the item from the toDoList array
+                    break;      // Breaking the loop after removing the item
+                }
+
+                else{
+                    console.error("Error: Item not found");      // Item not found
+                    break;
+                }
+            }
+            //Another method other than using for loop- find method
+           /*  toDoList.find((elem, index) => {
+                if(elem === deleteThisItem){
+                    /toDoList.splice(index, 1);   // Removing the item from the toDoList array
+                else{
+                    console.error("Error: Item not found");      // Item not found}
+                }
+            }); */
+            let newToDo = toDoList;     // Reference to the existing toDoList array
+            newToDo = newToDo.filter(item => item !== body.item);        // Removing the specified to-do item from the toDoList array
+            console.log(newToDo);    // Logging the updated toDoList to the console
+        });
+}
+    //DATA TRANSFERED IN HEXADECIMAL FORMAT 
+        //NOTE: while making POST request we use JSON format, and always write data in strings(double quotes)
+    else{
+        res.writeHead(501);          //501 means not implemented
+    }
+}
+
+else{
+    res.writeHead(404);              // 404 means not found
 }
     res.end();                       // Ending the response without sending any data    
     
@@ -46,7 +107,7 @@ if(url === "/todos"){
     console.log(`NodeJs Server Started Running on Port ${port}`);  // Logging a message when the server starts
 })
 
-/* http://localhost:8081            give this link in the browser */
+/* http://localhost:8081/todos            give this link in the browser */
 
 
 // This is a simple HTTP server that listens on port 8081. It does not handle any requests yet.
@@ -64,3 +125,9 @@ if(url === "/todos"){
 // http://localhost:8081/ContactUs
 // http://localhost:8081/AboutUs
 
+/*NOTE: IN ONE SERVER SESSION, YOU CAN DO AS MANY POST REQUESTS AS YOU WANT
+AND WHEN YOU CLICK GET METHOD, ALL THE DATA IS SHOWN(EVEN SAME ITEM REPEATED)
+
+BUT AS SOON AS YOU RESTART THE SERVER, THE DATA IS LOST.
+SO TO MAKE IT PERSISTENT, WE USE DATABASES LIKE MONGODB, MYSQL, ETC. 
+*/
